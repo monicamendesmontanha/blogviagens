ab3396fe52da5db861a458d0c8ca6ccaed546782
Mônica Mendes <monicamendesmontanha@gmail.com>
2017-11-19 22:37:21 +1100

    add view engine

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+app.set("view engine", "ejs")
 
 app.get("/", function(request, response) {
-    response.send("Hello!")
-})
+    response.render("pages/index");
+});
+
+app.get("/destinos", function(request, response) {
+    response.render("pages/destinos");
+});
 
 app.listen(port, function() {
     console.log(`listening on port ${port}`);
