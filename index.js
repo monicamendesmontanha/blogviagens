3ff9e1df0adb2b15a558e50e914e9255ed59e8e8
Mônica Mendes <monicamendesmontanha@gmail.com>
2017-11-19 21:48:10 +1100

    add new html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"))
+
 app.get("/", function(request, response) {
     response.send("Hello!")
 })
