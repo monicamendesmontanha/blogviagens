6c47d0e4e1c36965a51d92db658c303c20304a65
Mônica Mendes <monicamendesmontanha@gmail.com>
2017-11-20 22:10:19 +1100

    add momento format data

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const moment = require("moment");
 const app = express();
 const port = 3000;
+const date = moment().format('dddd, D MMMM YYYY')
 
 app.use(express.static("public"));
 
 app.set("view engine", "ejs")
 
 app.get("/", function(request, response) {
-    response.render("pages/index");
+    response.render("pages/index", {date: date});
 });
 
 app.get("/destinos", function(request, response) {
-    response.render("pages/destinos");
+    response.render("pages/destinos", {date: date});
 });
 
 app.listen(port, function() {
